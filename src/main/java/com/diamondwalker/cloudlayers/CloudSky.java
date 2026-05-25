d4904cddaef508f0c218fb862c02539b9bcb0325
DiamondWalker <19913710+DiamondWalker@users.noreply.github.com>
2026-05-25 06:53:54 -0400

    Lag fix

--- src/main/java/com/diamondwalker/cloudlayers/CloudSky.java
+++ src/main/java/com/diamondwalker/cloudlayers/CloudSky.java
@@ -1,6 +1,7 @@
 package com.diamondwalker.cloudlayers;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexBuffer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.DimensionSpecialEffects;
@@ -9,6 +10,7 @@ import org.joml.Matrix4f;
 
 public class CloudSky extends DimensionSpecialEffects.OverworldEffects {
     private int layer = 0;
+    private CloudLayerCache[] layers = new CloudLayerCache[0];
 
     @Override
     public float getCloudHeight() {
@@ -20,8 +22,19 @@ public class CloudSky extends DimensionSpecialEffects.OverworldEffects {
         LevelRenderer renderer = Minecraft.getInstance().levelRenderer;
 
         if (layer == 0) {
+            if (Config.LAYER_COUNT.get() != layers.length) {
+                layers = new CloudLayerCache[Config.LAYER_COUNT.get()];
+            }
+
             while (layer < Config.LAYER_COUNT.get()) {
-                layer++;
+                int index = layer++; // index starts at 0, layer starts at 1
+
+                if (layers[index] == null) {
+                    layers[index] = new CloudLayerCache();
+                    layers[index].copy(renderer);
+                }
+
+                layers[index].load(renderer);
 
                 double total = ((double)partialTick + ticks) * (getCloudHeight() / super.getCloudHeight()); // scale speed by the height
                 total += (layer - 1) * 10_000; // add an offset so they don't all start lined up
@@ -29,13 +42,38 @@ public class CloudSky extends DimensionSpecialEffects.OverworldEffects {
                 renderer.ticks = (int)Math.floor(total);
                 float partial = (float)(total - renderer.ticks);
                 renderer.renderClouds(poseStack, modelViewMatrix, projectionMatrix, partial, camX, camY, camZ);
+
+                layers[index].copy(renderer);
             }
             layer = 0;
             renderer.ticks = ticks;
+
+            layers[0].load(renderer);
 
             return true;
         }
 
         return false;
+    }
+
+    private static class CloudLayerCache {
+        private int prevCloudX;
+        private int prevCloudY;
+        private int prevCloudZ;
+        private VertexBuffer buffer;
+
+        void copy(LevelRenderer renderer) {
+            prevCloudX = renderer.prevCloudX;
+            prevCloudY = renderer.prevCloudY;
+            prevCloudZ = renderer.prevCloudZ;
+            buffer = renderer.cloudBuffer;
+        }
+
+        void load(LevelRenderer renderer) {
+            renderer.prevCloudX = prevCloudX;
+            renderer.prevCloudY = prevCloudY;
+            renderer.prevCloudZ = prevCloudZ;
+            renderer.cloudBuffer = buffer;
+        }
     }
 }
